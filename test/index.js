d43dc6c6462a8f3a3974fcbe326b6f9a549f9022
Alexander Beletsky <alexander.beletsky@gmail.com>
2018-03-15 19:32:26 +0100

    initial setup

--- test/index.js
+++ test/index.js
@@ -1,1 +1,9 @@
-// empty for now
+/* eslint prefer-arrow-callback: 0 */
+
+import expect from 'expect';
+
+describe('spec', function () {
+  it('should fail', function () {
+    expect(1).toEqual(0);
+  });
+});
